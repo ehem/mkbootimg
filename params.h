51dae1906fcfc6a3184d1c479dcf13e309a52cf9
Elliott Mitchell <ehem+github@m5p.com>
2019-03-04 13:39:06 -0800

    Add header file for common options
    Ensure options/values remain the same between the pack and unpack
    commands.  By using a common file they're guaranteed to remain
    synchronized.

---
+++ params.h
@@ -0,0 +1,40 @@
+/* params.h
+**
+** Copyright 2019, Elliott Mitchell
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**     http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*/
+
+
+#ifndef __PARAMS_H__
+#define __PARAMS_H__
+
+#define BASE_OPT	"base"
+#define BOARD_OPT	"board"
+#define CMDLINE_OPT	"cmdline"
+#define DT_OPT		"dt"
+#define HASH_OPT	"hash"
+#define HEADER_VERS_OPT	"header_version"
+#define KERNEL_OPT	"kernel"
+#define KERNEL_OFF_OPT	"kernel_offset"
+#define OS_PATCH_OPT	"os_patch_level"
+#define OS_VER_OPT	"os_version"
+#define PAGE_OPT	"pagesize"
+#define RAMDISK_OPT	"ramdisk"
+#define RAMDISK_OFF_OPT	"ramdisk_offset"
+#define RECOVERY_DT_OPT	"recovery_dtbo"
+#define SECOND_OFF_OPT	"second_offset"
+#define SECOND_OPT	"second"
+#define TAGS_OFF_OPT	"tags_offset"
+
+#endif
